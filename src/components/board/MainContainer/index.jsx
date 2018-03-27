4b796510dbacf8fb7b5d5d0562fed1110e0a43e8
Miniaylo.S <po4tovuj@gmail.com>
2018-03-27 20:30:57 +0300

    add header

--- src/components/board/MainContainer/index.jsx
+++ src/components/board/MainContainer/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '@/components/board/Header';
 import './styles.scss';
 
 import PostList from "../PostList";
@@ -6,9 +7,10 @@ import PostList from "../PostList";
 export default class MainContainer extends Component {
   render() {
     return (
-      <div className="post-wrapper--middle-grey">
-        <PostList />
-      </div>
+        <div className="post-wrapper--middle-grey">
+            <Header />
+            <PostList />
+        </div>
     );
   }
 }
